22a5312734e33263ee9fb80b5da22f44f2a8dcc8
welisonvidal93@gmail.com <welisonvidal93@gmail.com>
2023-05-16 18:56:40 -0400

    formulario

---
+++ script.js
@@ -0,0 +1,13 @@
+function limpar() {
+  document.getElementById("nome").value = "";
+  document.getElementById("sobrenome").value = "";
+  document.getElementById("cpf").value = "";
+  document.getElementById("nascimento").value = "";
+  document.getElementById("telefone").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("rua").value = "";
+  document.getElementById("numero").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("estado").value = "";
+}
